045fc19421af3cf73d186654843ac2002a64c34d
delberttt <delbertfelix@gmail.com>
2021-04-21 22:00:46 +0800

    create ParkingException

---
+++ src/main/java/ParkingApp/ParkingException/ParkingException.java
@@ -0,0 +1,15 @@
+package ParkingApp.ParkingException;
+
+public class ParkingException extends Exception {
+
+    public ParkingException()
+    {
+        super();
+    }
+
+    public ParkingException (String errorMessage, Throwable err)
+    {
+        super(errorMessage, err);
+    }
+
+}
